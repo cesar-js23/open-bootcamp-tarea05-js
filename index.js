fd40e437c3b87cb9a82caf33c4005724d930681c
Cesar Julian <trabajos.cesar23@gmail.com>
2022-08-05 22:45:20 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+let alturaCentimetros = 161;
+let alturaMetros = 1.61;
+let pesoKilogramos = 77.5;
+
+
+let alturaMetrosRedondeada = Math.ceil(alturaMetros);
+let pesoKilogramosRedondeada = Math.floor(pesoKilogramos);
+
+let sonIguales = Number.MAX_VALUE + 1 === Number.MAX_VALUE
